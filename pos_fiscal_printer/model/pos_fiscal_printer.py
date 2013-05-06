f3fc6f4e61bbff0b0a13d3de0edb164ec2337d9a
Eduardo Ochoa <elos3000@gmail.com>
2013-05-06 18:21:15 -0430

    [IMP] Agregada funcionalidad para leer el serial Agregada interfaz y modelo para los metodos de pago

--- pos_fiscal_printer/model/pos_fiscal_printer.py
+++ pos_fiscal_printer/model/pos_fiscal_printer.py
@@ -39,27 +39,23 @@ class printer(osv.Model):
         pb_obj = self.pool.get('pos_fiscal_printer.printer_brand')
         pm_obj = self.pool.get('pos_fiscal_printer.printer_model')
         for brand in printers:
-            brand_id = pb_obj.search(cr,uid,[('brand_name','=',brand)],context=context)
+            brand_id = pb_obj.search(cr,uid,[('brand_name','=',brand)],
+                            context=context)
             if not brand_id:
-                brand_id = [pb_obj.create(cr,uid,{'brand_name':brand},context)]
+                brand_id = [pb_obj.create(cr,uid,{'brand_name':brand},
+                            context)]
             for model in printers[brand]:
                 m = pm_obj.search(cr,uid,[('model_name','=',model)],
                         context=context,count=True)
                 if (m ==0):
-                    pm_obj.create(cr,uid,{'brand_id':brand_id[0],'model_name':model},context)
-            
-    
-    def _get_brands(self,cursor,user_id,context=None):
-                
-        url = "http://localhost:8069/pos/get_supported_brands"    
-        req = urllib2.Request(url)
-        try:
-            res = urllib2.urlopen(req)
-        except:
-            print "Not found 404"
-        brands = json.loads(res.read())
-        return [(brand,brand) for brand in brands]
+                    pm_obj.create(cr,uid,{'brand_id':brand_id[0],
+                        'model_name':model},context)
         
+    def read_serial(self, cr, uid, ids, context=None):
+        context = context or {}
+        print "making something cool"
+        pdb.set_trace()
+        return None
          
     _columns = {
         'name' : fields.char(string='Name', size=50, required=True),
@@ -69,9 +65,24 @@ class printer(osv.Model):
             string='Model',required=True), 
         'port' : fields.char(string='Port', size=100, required=True),
         'type': fields.boolean('Ticket Printer'),
-        'serial' : fields.char(string='Serial', size=50, required=True),
-        'active' : fields.boolean("Active"),            
+        'serial' : fields.char(string='Serial', size=50),
+        'active' : fields.boolean("Active"),
+        'payment_method_ids' : fields.one2many('pos_fiscal_printer.payment_method',
+            'printer_id',string="Payment Methods"),            
     }
-class tax_rates(osv.Model):
-    _name = 
     
+class payment_method(osv.Model):
+    _name = 'pos_fiscal_printer.payment_method'
+    _columns = {
+        'printer_id': fields.many2one('pos_fiscal_printer.printer'),   
+        'account_journal_id': fields.many2one('account.journal',
+            string='Payment Method',domain=[('journal_user','=','True')]),
+        'payment_method_id': fields.char(string='Id',size=2),
+        'description':fields.char(string='Description', size=50),
+        'current_description':fields.char(string='Current Description', size=50),        
+    }
+    
+    _defaults = {
+        'current_description': lambda *a: "Not available"
+    }
+        
