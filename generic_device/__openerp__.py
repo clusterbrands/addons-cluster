04726377b7225f3bb10ab3bb730a1f4915f87ce1
Eduardo Ochoa <eduardo.ochoa@clusterbrands.com.ve>
2013-07-22 11:34:27 -0430

    [IMP] -generic_device class created

---
+++ generic_device/__openerp__.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+###########################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#    Copyright (C) OpenERP Venezuela (<http://openerp.com.ve>).
+#    All Rights Reserved
+###############Credits######################################################
+#    Coded by: Eduardo Ochoa    <eduardo.ochoa@clusterbrands.com.ve>
+#                               <elos3000@gmail.com>
+#############################################################################
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+{
+    'name' : "Generic Device"
+    'category' : "Test",
+    'version' : "1.0",
+    'author' : "Me",
+    'description' : """Generic Device interface""",
+    #'init_xml': [
+    #    'data/fiscal_printer_data.xml'
+    # ],
+    #'data' : [        
+    #    'view/fiscal_printer_view.xml',
+    #    'view/fiscal_printer_action_menu.xml',
+    #],
+}
