b4eebaf6e7f544c288bd8d783827b58599dc2128
Luis Escobar <luis@vauxoo.com>
2013-09-02 16:51:56 -0430

    [IMP] deleted unused field. [IMP] Added field to show instrument in bank statement.

--- payment_instrument/model/account_bank_statement.py
+++ payment_instrument/model/account_bank_statement.py
@@ -8,8 +8,6 @@ from osv import fields
 class account_bank_statement(osv.Model):
     _inherit = 'account.bank.statement'
     _columns = {
-            'code':fields.related('instrument_id', 'code', 'Code',
-                help="""Reference Code of Payment Instrument"""),
             'instrument_id':fields.many2one('payment_instrument.instrument',
                 'Payment Instrument', help="""Payment Instrument linked to Statement"""),
     }
