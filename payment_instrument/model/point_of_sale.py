d26206c17f8a38505f63c347c52bda62d3f5ac6e
Luis Escobar <luis@vauxoo.com>
2013-09-04 15:27:01 -0430

    [IMP] Added statement into instrument to show 'Summary by Payment Methods' correctly [IMP] Overwritten add_payment method to push statement id appropiated

--- payment_instrument/model/point_of_sale.py
+++ payment_instrument/model/point_of_sale.py
@@ -1,3 +1,9 @@
+from datetime import datetime                                                                       
+from dateutil.relativedelta import relativedelta                                                    
+from decimal import Decimal                                                                         
+import logging                                                                                      
+import pdb                                                                                          
+import time 
 
 from openerp.osv import fields, osv
 from openerp import tools
@@ -59,3 +65,49 @@ class pos_session(osv.Model):
             bank_statement_ids)]}, context=context)
 
         return res 
+
+class pos_order(osv.Model):
+    _inherit = 'pos.order'
+
+    def add_payment(self, cr, uid, order_id, data, context=None):
+        """Create a new payment for the order"""                                                    
+        if not context:                                                                             
+            context = {}                                                                            
+        statement_line_obj = self.pool.get('account.bank.statement.line')                           
+        property_obj = self.pool.get('ir.property')                                                 
+        order = self.browse(cr, uid, order_id, context=context)                                     
+        args = {                                                                                    
+            'amount': data['amount'],                                                               
+            'date': data.get('payment_date', time.strftime('%Y-%m-%d')),
+            'name': order.name + ': ' + (data.get('payment_name', '') or ''),                       
+        }                                                                                           
+                                                                                                    
+        account_def = property_obj.get(cr, uid, 'property_account_receivable', 'res.partner', context=context)
+        args['account_id'] = (order.partner_id and order.partner_id.property_account_receivable \
+                             and order.partner_id.property_account_receivable.id) or (account_def and account_def.id) or False
+        args['partner_id'] = order.partner_id and order.partner_id.id or None
+                                                                                                    
+        if not args['account_id']:                                                                  
+            if not args['partner_id']:                                                              
+                msg = _('There is no receivable account defined to make payment.')                  
+            else:                                                                                   
+                msg = _('There is no receivable account defined to make payment for the partner: "%s" (id:%d).') % (order.partner_id.name, order.partner_id.id,)
+            raise osv.except_osv(_('Configuration Error!'), msg)                                    
+                                                                                                    
+        context.pop('pos_session_id', False)                                                        
+                                                                                                    
+        journal_id = data.get('journal', False)                                                     
+        statement_id = data.get('statement_id', False)                                              
+        assert journal_id or statement_id, "No statement_id or journal_id passed to the method!" 
+
+        args.update({                                                                               
+            'statement_id' : statement_id,                                                          
+            'pos_statement_id' : order_id,                                                          
+            'journal_id' : journal_id,                                                              
+            'type' : 'customer',                                                                    
+            'ref' : order.session_id.name,                                                          
+        })                                                                                          
+                                                                                                    
+        statement_line_obj.create(cr, uid, args, context=context)                                   
+                                                                                                    
+        return statement_id
