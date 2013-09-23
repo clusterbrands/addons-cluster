1c00d7f3c9bc73f6a141564fb5fc7226311a8ba8
Eduardo Ochoa <eduardo.ochoa@clusterbrands.com.ve>
2013-09-23 12:06:55 -0430

    [REM] Removed unused qweb templates

--- pos_client/static/src/js/widgets.js
+++ pos_client/static/src/js/widgets.js
@@ -6,13 +6,13 @@ function pos_client_widgets(instance, module){
             
             this.customer_form = new module.CustomerForm(this, {draggable:false});
             this.customer_form.appendTo($('.point-of-sale'));
-            
+            this.screen_selector.add_popup('customer-form',this.customer_form)
+
             this.select_customer_button = new module.HeaderButtonWidget(this,{
                 label:'Customer',
                 action: function(){self.screen_selector.show_popup('customer-form');},
             });
-            this.select_customer_button.appendTo(this.$('#rightheader'));
-            this.screen_selector.add_popup('customer-form',this.customer_form)
+            this.select_customer_button.appendTo(this.$('#rightheader'));            
         }
     })
 }
