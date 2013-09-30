325651a37acd0ced78d9879c137d2c6eeb7e8d42
Eduardo Ochoa <eduardo.ochoa@clusterbrands.com.ve>
2013-09-30 10:20:05 -0430

    [REF] -Updated module description

--- cash_count/__openerp__.py
+++ cash_count/__openerp__.py
@@ -1,15 +1,17 @@
 
+#!/usr/bin/python
 # -*- encoding: utf-8 -*-
-#
+###########################################################################
 #    Module Writen to OpenERP, Open Source Management Solution
 #    Copyright (C) OpenERP Venezuela (<http://openerp.com.ve>).
 #    All Rights Reserved
-# Credits######################################################
-#    Coded by: Coorporacion ClusterBrands C.A
-#
+###############Credits######################################################
+#    Coded by: Eduardo Ochoa  <eduardo.ochoa@clusterbrands.com.ve>
+#                    
+#############################################################################
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
@@ -19,18 +21,33 @@
 #
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
 {
     'name': "Cash Count",
     'category': "Generic Modules/Accounting",
     'version': "1.0",
-    'depends': ['point_of_sale','pos_base'],
+    'depends': ['hr','pos_base'],
     'author': "Coorporacion ClusterBrands C.A",
     'description': """
+
+This module allows you to manage the cashiers and cash counts in the POS
+interface
+
+Main features
+-------------
+    * Create and select cashiers
+    * Open a cashbox
+    * Lock/Unlock a existing session
+    * Generate a X report
+    * Generate a Z report
+
+Note:
+-----
+
 """,
     'data': [
         'view/cash_count_action_menu.xml',
         'view/cash_count_view.xml',
+        'view/hr_view.xml',
     ],
     'js': [
         'static/src/js/backbone-super-min.js',
--- cash_count/model/__init__.py
+++ cash_count/model/__init__.py
@@ -1,1 +1,2 @@
+import hr
 import cash_count
