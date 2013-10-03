b37fb052a4a24cbf99d5c41c64086f0af5054a65
Eduardo Ochoa <eduardo.ochoa@clusterbrands.com.ve>
2013-10-03 10:39:55 -0430

    [REM] -Removed model cashier

--- cash_count/model/cash_count.py
+++ cash_count/model/cash_count.py
@@ -2,19 +2,3 @@ from openerp.osv import osv, fields
 from openerp.tools.translate import _
 
 
-class cashier(osv.Model):
-    _name = "cash.count.cashier"
-
-    def validate(self, cr, uid, name, password, context=None):
-        context = context or {}
-        ids =  self.search(cr, uid, [('name','=',name),('password','=',password)], context=context)
-        if ids:
-            return self.read(cr, uid, ids[0], ['name'], context=context)
-        else:
-            return {}
-
-    _columns = {
-        "name":fields.char(string="Name", size=50, required=True),
-        "password":fields.char(string="Password", size=50,
-                               required=True),
-    }
