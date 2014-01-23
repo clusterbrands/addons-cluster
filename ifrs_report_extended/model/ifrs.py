db3642a41e0c75a4f4c3073067974de9be7c9b05
Eduardo Ochoa <eduardo.ochoa@clusterbrands.com.ve>
2014-01-23 11:47:41 -0430

    [REF] -Actualizada traduccion (es) -update translation (es)

---
+++ ifrs_report_extended/model/ifrs.py
@@ -0,0 +1,20 @@
+from osv import osv
+from osv import fields
+from tools.translate import _
+
+class ifrs_ifrs(osv.osv):
+    _inherit = 'ifrs.ifrs' 
+    #Do not touch _name it must be same as _inherit
+    #_name = 'ifrs.ifrs' 
+
+    def _get_period_print_info(self, cr, uid, ids, period_id, report_type, context=None):
+
+        if context is None:
+            context = {}
+        if report_type == 'all':
+            res = _('ALL PERIODS OF THE FISCALYEAR ')
+        else:
+            period = self.pool.get('account.period').browse(
+                cr, uid, period_id, context=context)
+            res = str(period.name) + ' [' + str(period.code) + ']'
+        return res
