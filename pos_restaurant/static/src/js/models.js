503e5f14ebd6d84dc76d5c72f8f738d56aab79eb
joseperez-cluster <josejperezm@cb-wrk-02.clusterbrands.com>
2014-06-06 15:13:52 -0430

    [IMP] Agregadas mejoras que se encontraban el el repositorio de bazaar

--- pos_restaurant/static/src/js/models.js
+++ pos_restaurant/static/src/js/models.js
@@ -1,24 +1,54 @@
 function pos_restaurant_models(instance, module){
-
+    
     module.PosModel = module.PosModel.extend({
         load_server_data : function(){
-            self = this
+            var self= this;
             loaded = this._super().then(function(){
                 return self.fetch('pos_restaurant.product_property',[],[]);
             }).then(function(properties){
-                var product_properties = new module.ProductPropertiesCollection(properties)
-                self.set('product_properties',product_properties);
-            });
-            return loaded 
+                self.add_product_properties(properties);
+            });;
+            return loaded
         },
         fetch: function(model, fields, domain, ctx){
             if (model == 'product.product')
                 fields.push('property_ids')
             return new instance.web.Model(model).query(fields).filter(domain).context(ctx).all()
         },
-    }); 
+        add_product_properties: function(properties){
+            var self= this;
+            var product_properties = new module.ProductPropertyList();
+            _.each(properties, function(property){
+                var products = new Backbone.Collection();
+                _.each(property.product_ids, function(id){
+                    products.add(self.db.get_product_by_id(id));
+                });
+                product_properties.push({
+                    'id': property.id,
+                    'name': property.name,
+                    'single_choice': property.single_choice,
+                    'sequence': property.sequence,
+                    'products': products,
+                })
+            });
+            this.set('product_properties', product_properties);
+        }
+    });
 
-    module.ProductPropertiesCollection = Backbone.Collection.extend({
+    module.ProductProperty = Backbone.Model.extend({
+        defaults: {
+            'name': '',
+            'single_choice': false,
+            'sequence': 0,
+            'products': new Backbone.Collection(),
+        },
+        initialize :function(attrs){
+            this._super(attrs);
+        },
+    });
+
+    module.ProductPropertyList = Backbone.Collection.extend({
+        model: module.ProductProperty,
         sort_key: 'id',
         comparator: function(item) {
             return item.get(this.sort_key);
@@ -26,6 +56,15 @@ function pos_restaurant_models(instance, module){
         sortByField: function(fieldName) {
             this.sort_key = fieldName;
             this.sort();
-        }
+        },
+    });
+
+    // module.Propertyline = module.Orderline.extend({
+    // });
+
+    module.Orderline = module.Orderline.extend({
+        initialize :function(attrs, options){
+            this._super(attrs, options);
+        },
     });
 }
--- pos_restaurant/static/src/js/widgets.js
+++ pos_restaurant/static/src/js/widgets.js
@@ -1,12 +1,5 @@
 function pos_restaurant_widgets(instance, module){
-
-    module.PosWidget.include({
-        init:function(parent,options){
-            this._super();
-            this.product_cache = new module.ImageCache();
-        },
-    })
-  
+    
     module.ProductListWidget.include({
         init: function(parent, options){
             var self = this
@@ -14,220 +7,70 @@ function pos_restaurant_widgets(instance, module){
                 if(product.get('to_weight') && self.pos.iface_electronic_scale){
                     self.pos_widget.screen_selector.set_current_screen(self.scale_screen, {product: product});
                 }else{
+                    self.pos.get('selectedOrder').addProduct(product);
                     if (product.get('property_ids').length > 0)
-                        self.show_selector(product)
-                    else
-                        self.pos.get('selectedOrder').addProduct(product);
+                        self.show_selector(product);
                 }
             },
-            this._super(parent, options)
+            this._super(parent, options);
         },
         show_selector: function(product){
             property_selector = new module.PropertySelector(this,{product:product,draggable:false})
-            property_selector.appendTo($('.point-of-sale'));
-            property_selector.show();               
+            property_selector.appendTo($('.point-of-sale'));             
         },
     });
 
     module.PropertySelector = module.BasePopup.extend({
-        template:'PropertySelector',
-        events:{
-            "click button[name='done']":"onClickBtnDone",
-            "click button[name='next']":"onClickBtnNext",
-            "click button[name='add']":"onClickBtnAdd",
-            "click button[name='del']":"onClickBtnDel",
-            "click a.delete-payment-line":"onClickBtnDelete",
-            "click button[name='previous']":"onClickBtnPrevious", 
-            "change input.name":"onChangeName",
-            "change input.rif":"onChangeRif",          
-        },
+        template: 'PropertySelector',
         init: function(parent, options){
-            var self = this
-            this._super(parent, options)
+            this._super(parent, options);
             this.step = 0;
-            this.product = options.product;
-            this.product_properties = this.loadProperties(options.product);  
-            this.current = this.product_properties.at(this.step);
-            this.productwidgets = [];
-            this.orderNames = new Backbone.Collection();
-            this.steps = this.product_properties.length;
-            this.last = this.steps - 1;     
+            this.productPropertyWidgets = []
+            this.orderLine = this.pos.get('selectedOrder').getSelectedLine();
+            this.product_properties = this.load_properties();
+            this.current = this.product_properties.at(0);
+        },
+        load_properties: function(){
+            var self = this;
+            var product = this.orderLine.get_product();
+            product_properties = this.pos.get('product_properties');
+            properties = new module.ProductPropertyList();
+            _.each(product.get('property_ids'), function(id){
+                properties.push(product_properties.get(id));
+            })
+            return product_properties;
         },
         renderElement: function(){
-            var self=this;
             this._super();
-            for(var i = 0, len = this.productwidgets.length; i < len; i++){
-                this.productwidgets[i].destroy();
-            }
-            this.productwidgets = []; 
-          
-            _(this.current.get('optional_products')).each(function(p) {         
-                var product = new module.CustomProductWidget(self, {
-                    model: p,
-                    click_product_action: self.click_product_action,
-                });
-                self.productwidgets.push(product);
-                product.on('select',self,self.select);
-                product.on('unSelect',self,self.unSelect);
-                product.appendTo(self.$('.product-list'));                      
-            });
-        },
-        loadProperties : function(product){
             var self = this;
-            var properties = new module.ProductPropertiesCollection();
-            var product_properties = self.pos.get('product_properties');
-            _(product.get('property_ids')).each(function(id) {
-                var property = product_properties.get(id).clone();
-                var products = self.pos.get('products');
-                var optional_products = []
-                _.each(property.get('optional_product_ids'), function(p){
-                    var product  = products.get(p).clone();
-                    product.set('selected',false);
-                    optional_products.push(product);
-                });
-                property.set('selected_products', []);
-                property.set('optional_products', optional_products);
-                properties.push(property);
-            });
-
-            var summary = new Backbone.Model();
-            summary.set('name',"Order Summary");
-            summary.set('selected_products',[]);
-
-            var customization = new Backbone.Model();
-            customization.set('name',"Order Customization");
-            customization.set('selected_products',[]);
-
-            properties.push(summary);
-            properties.push(customization);
-            properties.sortByField('sequence')
-            return properties
-        },
-        select: function(product, widget){
-            if (this.current.get('single_choice')){
-                _.each(this.productwidgets,function(w){
-                    w.unSelect();
-                });
-                this.current.set('selected_products',[product]);
-            }else
-                this.current.get('selected_products').push(product);
-            widget.select();
-        },
-        unSelect: function(product){
-            var selected_products = this.current.get('selected_products');
-            this.current.set('selected_products',_.without(selected_products,product));
-        },
-        getTotal: function(){
-            var subtotal = 0.0;
-            _.each(this.product_properties.models, function(property){
-                _.each(property.get('selected_products'), function(product){
-                    subtotal+= product.get('price');
-                });
-            });
-            return subtotal + this.product.get('price');
-        },
-        set_step: function(value){
-            this.step = value;
-            this.current = this.product_properties.at(this.step);
-            this.renderElement();
-        },
-        onChangeRif: function(e){
-            var i = this.$(e.target).attr('index');
-            var val = this.$(e.target).val();
-            this.orderNames.at(i).set('rif',val);
-            p = this.pos.db.search_customer(module.country_code+val);
-            if (p){
-                this.$("input.name[index="+i+"]").val(p.name);
-                this.orderNames.at(i).set('name',p.name);
+            
+            for(var i = 0, len = this.productPropertyWidgets.length; i < len; i++){
+                this.productPropertyWidgets[i].destroy();
             }
+            this.productPropertyWidgets = []; 
+            _.each(this.current.get('products').models, function(p){
+                var widget = new  module.ProductPropertyWidget(this, {model: p});
+                self.productPropertyWidgets.push(widget); 
+                widget.appendTo(self.$('.product-list'));                     
+            });           
         },
-        onChangeName: function(e){
-            var i = this.$(e.target).attr('index');
-            var val = this.$(e.target).val();
-            this.orderNames.at(i).set('name',val);
-        },
-        onClickBtnDelete: function(event){
-            var product_id = this.$(event.currentTarget).attr('product_id');
-            var property_id = this.$(event.currentTarget).attr('property_id');
-            var property = this.product_properties.get(property_id);            
-            var product =  _(property.get('optional_products')).find(function(p) {
-                return p.id == product_id;
-            })
-            product.set('selected',false);
-            selected_products  = property.get('selected_products');
-            property.set('selected_products',_.without(selected_products,product));
-            this.renderElement();
-        },
-        onClickBtnAdd: function(){
-            this.orderNames.push({
-                'rif': '',
-                'name':'',
-            });
-            this.renderElement();
-            this.$('input.rif:last-child').focus();
-        },
-        onClickBtnDel: function(){
-            this.orderNames.pop();
-            this.renderElement();
-            this.$('input.rif:last-child').focus();
-        },
-        onClickBtnDone: function(){
-            this.getTotal();
-            alert("Done");
-        },
-        onClickBtnPrevious: function(){
-            if (this.product_properties.at(this.step - 1)){
-                this.set_step(this.step - 1);
-            }
-        },
-        onClickBtnNext: function(){
-            if (this.product_properties.at(this.step + 1)){
-                this.set_step(this.step + 1);
-            }
+        closePopup:function(e){
+            //Pendiente: Si es edicion no se debe borrar esta linea
+            this.pos.get('selectedOrder').removeOrderline(this.orderLine);
+            this._super();
         },
     });
-
-    module.ProductWidget.include({
-        renderElement: function() {
-            this._super();
-            this.pos_widget.product_cache.get_image(this.model.get_image_url());
-        },
-    })
-
-    module.CustomProductWidget =  module.PosBaseWidget.extend({
-        template: 'CustomProductWidget',
+    
+    module.ProductPropertyWidget =  module.PosBaseWidget.extend({
+        template: 'ProductPropertyWidget',
         init: function(parent, options) {
             this._super(parent,options);
             this.model = options.model;
             this.model.attributes.weight = options.weight;
         },
-        on_click_action: function(){
-            this.model.set('selected',!this.model.get('selected'));
-            if (this.model.get('selected')){
-                this.trigger('select', this.model, this);
-            }else{
-                this.trigger('unSelect', this.model, this);
-                this.$el.removeClass('selected');
-            }
-        },
-        select:function(){
-            this.model.set('selected', true);
-            this.$el.addClass('selected');
-        },
-        unSelect: function(){ 
-            this.model.set('selected', false);
-            this.$el.removeClass('selected');
-        },
         renderElement: function(){
-            var self= this;
             this._super();
-            this.$('img').replaceWith(this.pos_widget.product_cache.get_image(this.model.get_image_url()));
-            if (this.model.get('selected')){
-                this.$el.addClass('selected');
-            }
-            $("a", this.$el).click(function(e){
-                self.on_click_action();
-            });
         },
     });
+    
 }
