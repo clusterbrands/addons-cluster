ef2363abe17840aa7ac0f2a8cd2d3b6194f0ebc9
Eduardo Ochoa <elos3000@gmail.com>
2016-01-21 09:28:55 -0430

    [FIX] account_financial_report_cluster -Corregidos problemas relacionados con el reporte.

--- account_financial_report_cluster/report/parser.py
+++ account_financial_report_cluster/report/parser.py
@@ -1,10 +1,10 @@
 from openerp.osv import osv
 from openerp.tools.translate import _
 from openerp.report import report_sxw
-from openerp.addons.account_financial_report.report.parser import account_balance 
+from openerp.addons.account_financial_report.report.parser import AccountBalance 
 from openerp.tools.safe_eval import safe_eval as eval
 
-class account_balance_cluster(account_balance):
+class account_balance_cluster(AccountBalance):
 
     def __init__(self, cr, uid, name, context):
         super(account_balance_cluster, self).__init__(cr, uid, name, context)
@@ -18,7 +18,9 @@ class account_balance_cluster(account_balance):
                 continue
             if line['total'] and line['label'] == False and form['partial_sumarize'] == False and  line.has_key('level'):
                 continue
-        	lines2.append(line)
+            import pdb
+            pdb.set_trace()
+            lines2.append(line)
         return lines2
 
 report_sxw.report_sxw('report.afr.rml.2cols2',
